684846f91aae085b730f461e5228544127d62e1e
hiccup90 <24733459+hiccup90@users.noreply.github.com>
2025-12-28 19:27:20 +0800

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -9,7 +9,8 @@ namespace SeasonMarkerCopier
     public class Plugin : BasePlugin<PluginConfiguration>
     {
         public override string Name => "Season Marker Copier";
-        public override Guid Id => Guid.Parse("7a8b9c1d-2e3f-4a5b-6c7d-8e9f0a1b2c3d"); // 固定一个GUID
+        public override Guid Id => Guid.Parse("7a8b9c1d-2e3f-4a5b-6c7d-8e9f0a1b2c3d");
+
         public static Plugin Instance { get; private set; }
 
         public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
@@ -18,6 +19,7 @@ namespace SeasonMarkerCopier
             Instance = this;
         }
 
-        public PluginConfiguration Configuration => base.Configuration;
+        // 修复点：改名为 InstanceOptions 并显式指向 base.Configuration
+        public PluginConfiguration InstanceOptions => base.Configuration;
     }
 }
