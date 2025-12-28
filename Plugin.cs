e06cfbc4b40d9a5af77ca6b2540e128455d9649d
hiccup90 <24733459+hiccup90@users.noreply.github.com>
2025-12-28 19:22:05 +0800

    Create Plugin.cs

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+using System;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Serialization;
+using SeasonMarkerCopier.Configuration;
+
+namespace SeasonMarkerCopier
+{
+    public class Plugin : BasePlugin<PluginConfiguration>
+    {
+        public override string Name => "Season Marker Copier";
+        public override Guid Id => Guid.Parse("7a8b9c1d-2e3f-4a5b-6c7d-8e9f0a1b2c3d"); // 固定一个GUID
+        public static Plugin Instance { get; private set; }
+
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+            : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        public PluginConfiguration Configuration => base.Configuration;
+    }
+}
