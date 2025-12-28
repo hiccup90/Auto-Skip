1862c213b8488be74850633b130a20485a94107a
hiccup90 <24733459+hiccup90@users.noreply.github.com>
2025-12-28 19:31:43 +0800

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -9,6 +9,7 @@ namespace SeasonMarkerCopier
     public class Plugin : BasePlugin<PluginConfiguration>
     {
         public override string Name => "Season Marker Copier";
+        // 请确保这个 GUID 是你专属的
         public override Guid Id => Guid.Parse("7a8b9c1d-2e3f-4a5b-6c7d-8e9f0a1b2c3d");
 
         public static Plugin Instance { get; private set; }
@@ -19,7 +20,7 @@ namespace SeasonMarkerCopier
             Instance = this;
         }
 
-        // 修复点：改名为 InstanceOptions 并显式指向 base.Configuration
+        // 这里的属性名改为 InstanceOptions，避免与父类的 Configuration 冲突
         public PluginConfiguration InstanceOptions => base.Configuration;
     }
 }
